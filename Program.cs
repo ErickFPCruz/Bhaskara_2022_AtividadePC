dd07d1090da1f7b6fc9859828be466e65516fc55
ErickFPCruz <erick.cruz97@hotmail.com>
2022-04-29 18:40:32 -0300

    Exercício demorado pra caramba...

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
 ﻿Console.Clear();
 
-double variavel1, variavel2, variaval3; //variavel1 = a, variavel2 = b, variavel3 = c
+double variavel1, variavel2, variaval3, discriminante;
+//variavel1 = a, variavel2 = b, variavel3 = c, discriminante = delta ()
 
+discriminante = variavel2(Math.Pow(2)) - 4 * variavel1 * variaval3;
+
+if (variavel1 == 0)
+{
+    Console.WriteLine("Não é possível realizar esta operação!");
+}
+else if (){
+
+        }
