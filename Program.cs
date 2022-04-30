8a5933b0deae450ff80b7e5715e61391318e9423
ErickFPCruz <erick.cruz97@hotmail.com>
2022-04-29 21:57:37 -0300

    Terminado tudo (último nível de Decisão)

--- Program.cs
+++ Program.cs
@@ -1,14 +1,38 @@
 ﻿Console.Clear();
 
-double variavel1, variavel2, variaval3, discriminante;
-//variavel1 = a, variavel2 = b, variavel3 = c, discriminante = delta ()
+double variavel1, variavel2, variavel3, discriminante, resultado1, resultado2;
+//variavel1 = a, variavel2 = b, variavel3 = c, discriminante = delta
 
-discriminante = variavel2(Math.Pow(2)) - 4 * variavel1 * variaval3;
+Console.Write("Insira o número A: ");
+variavel1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Insira o número B: ");
+variavel2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Insira o número C: ");
+variavel3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
 
 if (variavel1 == 0)
 {
     Console.WriteLine("Não é possível realizar esta operação!");
 }
-else if (){
+else
+{
+    discriminante = (Math.Pow(variavel2, 2)) - (4 * variavel1 * variavel3);
 
-        }
+    if (discriminante < 0)
+    {
+        Console.WriteLine("Não é possível realizar esta operação!");
+    }
+    else
+    {
+        resultado1 = (-variavel2 + Math.Sqrt(discriminante)) / (2 * variavel1);
+        Console.WriteLine($"x1 = {resultado1}");
+
+        resultado2 = (-variavel2 - Math.Sqrt(discriminante)) / (2 * variavel1);
+        Console.WriteLine($"x2 = {resultado2}");
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Operação Finalizada!");
